2b8b6bde8f459eec4e5ebe5a86cd6742c9ac1ca8
Marcos Stefani <marcos.stefani@m4u.com.br>
2020-07-03 11:10:06 -0300

    model

---
+++ 2_flask_api/api/model/model.py
@@ -0,0 +1,10 @@
+import json
+from db import db
+
+class Model:
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
